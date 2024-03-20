fa9191974a3f85ed9ea98ec73b80dd857cb4eae6
Lucas Liggi <lucasvechiliggi@gmail.com>
2024-03-20 15:25:20 -0300

    Modifica a parte de console fazendo a conta

--- aula01/TelaImc.java
+++ aula01/TelaImc.java
@@ -50,10 +50,10 @@ public class TelaImc extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNome = new JLabel("Nome");
-		lblNome.setFont(new Font("Arial", Font.PLAIN, 20));
-		lblNome.setBounds(10, 10, 65, 27);
-		contentPane.add(lblNome);
+		JLabel lbl1 = new JLabel("Nome");
+		lbl1.setFont(new Font("Arial", Font.PLAIN, 20));
+		lbl1.setBounds(10, 10, 65, 27);
+		contentPane.add(lbl1);
 		
 		JLabel lblNewLabel = new JLabel("Peso");
 		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 20));
@@ -66,44 +66,77 @@ public class TelaImc extends JFrame {
 		contentPane.add(lblNewLabel_1);
 		
 		tfNome = new JTextField();
+		tfNome.setHorizontalAlignment(SwingConstants.CENTER);
 		tfNome.setFont(new Font("Arial", Font.PLAIN, 20));
 		tfNome.setBounds(72, 10, 107, 25);
 		contentPane.add(tfNome);
 		tfNome.setColumns(10);
 		
 		tfAltura = new JTextField();
+		tfAltura.setHorizontalAlignment(SwingConstants.CENTER);
+		tfAltura.setText("0.0");
 		tfAltura.setFont(new Font("Arial", Font.PLAIN, 20));
 		tfAltura.setBounds(72, 61, 107, 25);
 		contentPane.add(tfAltura);
 		tfAltura.setColumns(10);
 		
 		tfPeso = new JTextField();
+		tfPeso.setHorizontalAlignment(SwingConstants.CENTER);
+		tfPeso.setText("0.0");
 		tfPeso.setFont(new Font("Arial", Font.PLAIN, 20));
 		tfPeso.setBounds(72, 116, 107, 25);
 		contentPane.add(tfPeso);
 		tfPeso.setColumns(10);
 		
+		JLabel lblPessoa = new JLabel("");
+		lblPessoa.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPessoa.setFont(new Font("Arial", Font.PLAIN, 20));
+		lblPessoa.setBounds(236, 4, 130, 40);
+		contentPane.add(lblPessoa);
+		
 		JLabel lblImc = new JLabel("");
 		lblImc.setFont(new Font("Arial", Font.PLAIN, 20));
 		lblImc.setHorizontalAlignment(SwingConstants.CENTER);
-		lblImc.setBounds(236, 73, 124, 39);
+		lblImc.setBounds(236, 62, 130, 40);
 		contentPane.add(lblImc);
+		
+		JLabel lblClasse = new JLabel("");
+		lblClasse.setHorizontalAlignment(SwingConstants.CENTER);
+		lblClasse.setFont(new Font("Arial", Font.PLAIN, 20));
+		lblClasse.setBounds(236, 136, 130, 40);
+		contentPane.add(lblClasse);
 		
 		JButton btnCalc = new JButton("Calcular");
 		btnCalc.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				double  altura = Integer.parseInt(tfAltura.getText());
-				double peso = Integer.parseInt(tfPeso.getText());
+				String nome = tfNome.getText();
+				float altura = Float.parseFloat(tfAltura.getText());
+				float peso = Float.parseFloat(tfPeso.getText());
 				DecimalFormat formatador = new DecimalFormat("#0.00");
 				
-				float resul = peso/(altura*altura);
-				
+				float resul = peso /(altura * altura);
+				lblPessoa.setText(nome);
 				formatador.format(resul);
-				lblImc.setText(Double.toString(resul));
+				lblImc.setText(String.valueOf(resul));
 				
+				//class
+				String magreza = ("Magreza");
+				String ideal = ("Ideal");
+				String sobrepeso = ("Sobrepeso");
+				String obeso = ("Obeso");
+				String obesidade = ("Obesidade grave");
 				
-				
-			
+				if (resul<= 18.5) {
+					lblClasse.setText(magreza);
+				}else if (resul<25) {
+					lblClasse.setText(ideal);
+				}else if (resul<30) {
+					lblClasse.setText(sobrepeso);
+				}else if (resul<40) {
+					lblClasse.setText(obeso);
+				}else {
+					lblClasse.setText(obesidade);
+				}
 			}
 		});
 		btnCalc.setFont(new Font("Arial", Font.PLAIN, 18));
@@ -113,8 +146,9 @@ public class TelaImc extends JFrame {
 		JLabel lblNewLabel_2 = new JLabel("Seu IMC é:");
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_2.setFont(new Font("Arial", Font.PLAIN, 20));
-		lblNewLabel_2.setBounds(226, 31, 134, 39);
+		lblNewLabel_2.setBounds(236, 35, 130, 40);
 		contentPane.add(lblNewLabel_2);
-
+		
+		
 	}
 }
