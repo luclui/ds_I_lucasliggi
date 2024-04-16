81452494b6b78686f885f833ca5c60833b4930d7
Lucas Liggi <lucasvechiliggi@gmail.com>
2024-04-16 20:14:11 -0300

    exercicio de fibonacci

--- aula03/ExFibonacci.java
+++ aula03/ExFibonacci.java
@@ -1,16 +1,17 @@
 package aula03;
 
 public class ExFibonacci {
-
-    public static void main(String[] args) {
-        int b;
-       
-        for (int i = 0; i < 9; i++) {
-         b = 0;
-         b = i+ (i - b)    ;
-        System.out.println(b);
-            
+    public static void main(String[] args){
+        int num1 = 1, num2 = 0;
+        
+        System.out.println(num2);
+        System.out.println(num1);
+        
+        for(int i = 0; i < 6; i++){
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            System.out.println(num1);
         }
-
     }
+    
 }
