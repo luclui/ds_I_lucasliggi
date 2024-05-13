3ef3891d38f1d73fb399e9d16810877bdb6e06e1
Lucas Liggi <lucasvechiliggi@gmail.com>
2024-05-13 08:51:56 -0300

    Aula sobre netbeans

---
+++ netbeans/Aluno/src/aluno/Aluno.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aluno;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Aluno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          AlunoObj aluno1;
+          aluno1 = new AlunoObj();
+          
+          
+          aluno1.setNome("Liggi");
+          aluno1.setIdade(16);
+          System.out.println(aluno1.getNome());
+          System.out.println(aluno1.getIdade());
+          System.out.println(aluno1.getAluno());
+    }
+    
+}
